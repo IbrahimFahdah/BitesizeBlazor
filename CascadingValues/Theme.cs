7b7ca7b1b2aacae59e7864d307b23caf86374cc9
i.fahdah@outlook.com <i.fahdah@outlook.com>
2020-01-15 16:59:50 +0000

    Adding CascadingValues bite

---
+++ CascadingValues/Theme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CascadingValues
+{
+    public class Theme
+    {
+        public string btClass { get; set; } ="btn-primary";
+        private bool _toggle;
+
+        public void ToggleTheme()
+        {
+            _toggle = !_toggle;
+            btClass = _toggle ? "btn-danger" : "btn-primary";
+        }
+    }
+}
