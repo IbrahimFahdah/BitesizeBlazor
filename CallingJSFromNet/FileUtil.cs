d8dd90832b4f55f717511745b2ea69a8b5657ccf
i.fahdah@outlook.com <i.fahdah@outlook.com>
2020-01-22 15:08:41 +0000

    Add a new bite to show how to show how to call JS function from .Net.

---
+++ CallingJSFromNet/FileUtil.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace CallingJSFromNet
+{
+    public static class FileUtil
+    {
+        public async static Task DownloadFile(IJSRuntime js, string filename, byte[] data)
+        {
+            await js.InvokeAsync<object>(
+                "download",
+                filename,
+                Convert.ToBase64String(data));
+        }
+
+
+    }
+}
